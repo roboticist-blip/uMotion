85435212978535efffe41bd4af5585ff4609d466
roboticist-blip <sumitmaheshwari812@gmail.com>
2025-10-06 19:34:13 +0530

    Update RawDiff.ino

--- examples/RawDiff/RawDiff.ino
+++ examples/RawDiff/RawDiff.ino
@@ -7,12 +7,12 @@ uMotion motion;
 void setup() {
   Serial.begin(115200);
   motion.begin(FRAMESIZE_QQVGA, PIXFORMAT_GRAYSCALE);
-  motion.setMode(MODE_RAW);       //  Raw difference visualization mode
-  motion.setThreshold(10);        //  Sensitivity threshold
+  motion.setMode(MODE_RAW);     
+  motion.setThreshold(10);        //Sensitivity threshold
   Serial.println("μMotion: RAW_DIFF mode started...");
 }
 
 void loop() {
-  motion.update();  // This will print "." for changed pixels and " " for unchanged ones
-  delay(100);       // Control frame rate for readability
+  motion.update();  
+  delay(100);       
 }
