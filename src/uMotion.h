4b9b3db1ac17547137ffa96e3c46988c3cbd70a4
Sumit maheshwari <sumitmaheshwari812@gmail.com>
2025-12-14 18:38:25 +0530

    Update uMotion.h

--- src/uMotion.h
+++ src/uMotion.h
@@ -13,33 +13,36 @@ enum uMotionMode {
 };
 
 class uMotion {
-  public:
-    bool begin(framesize_t size = FRAMESIZE_QQVGA,
-               pixformat_t format = PIXFORMAT_GRAYSCALE);
+public:
+  bool begin(framesize_t size = FRAMESIZE_QQVGA,
+             pixformat_t format = PIXFORMAT_GRAYSCALE);
 
-    void setMode(uMotionMode mode);
-    void setThreshold(uint8_t thresh);
-    void update();
+  void setMode(uMotionMode mode);
+  void setThreshold(uint8_t thresh);
+  void update();
 
-    bool motionDetected();
-    int  getMotionPixels();
-    float getMotionRatio();
-    unsigned long getLatency();
+  bool motionDetected();
+  int  getMotionPixels();
+  float getMotionRatio();
+  unsigned long getLatency();
 
-  private:
-    void calculateDifference();
-    void printRawDiff();
-    uMotionMode mode = MODE_SUMMARY;
-    uint8_t threshold = 10;
-    bool motion = false;
-    int motion_pixels = 0;
-    float motion_ratio = 0.0f;
-    unsigned long latency = 0;
-    uint8_t *prevFrame = nullptr;                 // Previous 96x96 frame
-    uint8_t procFrame[PROC_W * PROC_H];           // Current 96x96 frame
-    camera_fb_t *fb = nullptr;
-    unsigned long frame_count = 0;
-    unsigned long t_last_log = 0;
+private:
+  void calculateDifference();
+  void printRawDiff();
+
+  uMotionMode mode = MODE_SUMMARY;
+  uint8_t threshold = 10;
+  bool motion = false;
+  int motion_pixels = 0;
+  float motion_ratio = 0.0f;
+  unsigned long latency = 0;
+
+  uint8_t *prevFrame = nullptr;
+  uint8_t procFrame[PROC_W * PROC_H];
+  camera_fb_t *fb = nullptr;
+
+  unsigned long frame_count = 0;
+  unsigned long t_last_log = 0;
 };
 
 #endif
